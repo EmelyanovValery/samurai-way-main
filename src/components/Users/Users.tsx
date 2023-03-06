a77946797b34c56f3a64b35200fa0c14d597a417
EmelyanovValery~ <vbh10@yandex.ru>
2023-03-06 23:43:02 +0500

    55.1 lesson completed

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -27,6 +27,11 @@ class Users extends React.Component<UsersPropsType, any>{
     render() {
         return (
             <div>
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+                <span>4</span>
+                <span>5</span>
                 {this.props.usersPageData.users.map(user=>
                     <div key={user.id}>
                     <span>
