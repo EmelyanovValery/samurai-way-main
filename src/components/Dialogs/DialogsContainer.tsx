bc31078a23cb5c85c2dde798b0aab0244a8a234c
EmelyanovValery~ <vbh10@yandex.ru>
2023-08-16 23:37:23 +0500

    Bug fix.

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -15,30 +15,7 @@ type DialogsContainerPropsType = {
     // updateNewMessage:(newMessage:string) =>void
 }
 
-// const DialogsContainer = (props: DialogsContainerPropsType) => {
-//
-//
-//     return <StoreContext.Consumer>
-//         {(store) => {
-//             const state = store.getState()
-//             const onChangeMessageTextHandler = (newMessage: string) => {
-//                 store.dispatch(updateNewMessageAC(newMessage))
-//                 //props.updateNewMessage(newMessage.current.value)
-//                 // props.dispatch(updateNewMessageAC(newMessage.current.value))
-//
-//             }
-//
-//             const onClickAddMassage = () => {
-//                 store.dispatch(addMessageAC())
-//                 // props.addMessage()
-//                 //  props.dispatch(addMessageAC())
-//             }
-//             return <Dialogs messagePageData={state.messagesPages} addMessage={onClickAddMassage}
-//                      updateNewMessage={onChangeMessageTextHandler}
-//             />}}
-//     </StoreContext.Consumer>
-//
-// };
+
 const mapStateToProps = (state:AppStateType):{messagePageData:messagesPagesType,isAuth:boolean} => {
   return {
       messagePageData:state.messagesPages,
